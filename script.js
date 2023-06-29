79266a99053e64c8d13c8ff94d1019b45619e210
Alex Larios <alexlarios70@hotmail.com>
2023-06-29 01:09:21 -0600

    Cambios en la generacion de contraseña

--- script.js
+++ script.js
@@ -1,73 +1,163 @@
 
 // Assignment Code
+let charTypesText = ["Should it be numerical"
+  , "Should the password include special characters"
+  , "Should it include upper case letters"
+  , "should it include lower case letters",
+  "should it include numerical characaters"];
+
 var charTypesBool = [];
 var generateBtn = document.querySelector("#generate");
-var contraseña = [] ;
-var mayusculas = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z"]; 
+var contraseña = [];
+var contraseñaTemp = [];
+var mayusculas = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"
+  , "L", "M", "N", "Ñ", "O", "P", "Q", "R", "S", "T"
+  , "U", "V", "W", "X", "Y", "Z"];
+var minusculas = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
+  "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numeros = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var caracEspeciales = ["!", "@", "·", "#", "$", "%", "&", "/", "(", ")", "=", "?", "¿"];
 
-var numeros ;
-var especiales;
-var minusculas;
+
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var passwordText = document.getElementById("#password");
 
-  passwordText.value = password;
+  passwordText.innerHTML = contraseña;
 
 }
+
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", generarContra);
 
+//empieza la funciòn generar contraseña
 function generarContra() {
 
-
+//aqui empieza la parte de la funcion que válida los criterios
   let lenghtOfPassword = prompt("Choose the lenght of the password between 8 and 128 characters");
-  /** 
-  while (isNaN(lenghtOfPassword )){
-    lenghtOfPassword= prompt(" This is a Nan You have to choose a valid number between 8 and 128");
+
+  if (isNaN(lenghtOfPassword)) {
+
+    while (isNaN(lenghtOfPassword)) {
+      lenghtOfPassword = prompt(" This is a Nan You have to choose a valid number between 8 and 128");
+    }
+    console.log(lenghtOfPassword);
+
+  } else if (lenghtOfPassword < 8 || lenghtOfPassword > 128) {
+    while (lenghtOfPassword < 8 || lenghtOfPassword > 128) {
+      lenghtOfPassword = prompt("You have to choose a valid number between 8 and 128");
+    }
   }
 
-  while (lenghtOfPassword !=> 8 &&)
+  console.log("el tamaño de la contraseña es " + lenghtOfPassword);
 
-  */
-
-
-
-if (isNaN(lenghtOfPassword)){
+  for (var i = 0; i < charTypesText.length;) {
+    charTypesBool[i] = confirm(charTypesText[i]);
+    i++;
+    console.log(charTypesBool);
+  }
   
-  while (isNaN(lenghtOfPassword)) {
-  lenghtOfPassword= prompt(" This is a Nan You have to choose a valid number between 8 and 128");
+//aquì acaba la parte de la funcioon que validad los criterios
+
+  console.log("los valores del arrey son" + charTypesBool);
+
+
+
+  
+
+
+//empieza la parte de la funcion que genera la contraseña
+  for (var i = 0; i < lenghtOfPassword ;) {
+    while (contraseña.length != lenghtOfPassword) {
+      console.log("entro al ciclo for")
+      if (charTypesBool[1] == true) {
+        contraseña.push(caracEspeciales[Math.floor(Math.random() * caracEspeciales.length)]);
+        console.log("imprime 1")
+
+      }
+
+      if (charTypesBool[2] == true) {
+        contraseña.push(mayusculas[Math.floor(Math.random() * mayusculas.length)]);
+        console.log("imprime 2")
+      }
+
+
+
+      if (charTypesBool[3] == true) {
+        contraseña.push(minusculas[Math.floor(Math.random() * minusculas.length)]);
+        console.log("imprime 3")
+      }
+
+      if (charTypesBool[4] == true) {
+        contraseña.push(numeros[Math.floor(Math.random() * numeros.length)]);
+        console.log("imprime 4")
+      }
+
+    }
+
+
+    console.log(contraseña);
+    
+    i++
   }
-console.log(lenghtOfPassword);
-/** 
-  parseInt(lenghtOfPassword);
-  console.log(lenghtOfPassword);
-  */
-} else if(lenghtOfPassword < 8 || lenghtOfPassword>128){
-  while (lenghtOfPassword < 8 || lenghtOfPassword>128){
-  lenghtOfPassword= prompt("You have to choose a valid number between 8 and 128");
-  }
+
+  contraseñaTemp = contraseña.join("");
+  alert("Su contraseña es:  " + contraseñaTemp);
+  console.log(contraseñaTemp);
+
+
+contraseña.length = 0;
+//termina la funcion que genera la contraseña
 }
 
-console.log("el tamaño de la contraseña es " + lenghtOfPassword);
 
 
-let charTypesText = ["Should it be numerical"
-,"Should the password include special characters"
-, "Should it include upper case letters"
-, "should it include lower case letters",
-"should it include numerical characaters"];
 
-for (var i = 0; i < charTypesText.length; i++) {
-  charTypesBool[i] = confirm(charTypesText[i]);
-  console.log(charTypesBool);
-}
 
-console.log("los valores del arrey son" + charTypesBool);
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//ESTE CODIGO VAS DESPUES DE LA LINEA 77
+/*
 if (charTypesBool[0] == true)
 {
   console.log("esta entrando al if numerico");
@@ -85,6 +175,8 @@ console.log(contraseña.join(""));
 alert("Su contraseña numérica es:  " + contraseña.join(""));
 
 } 
+
+
 else if (charTypesBool[2]==true){
   console.log("esta entrando al else if ");
 
@@ -104,7 +196,4 @@ console.log(contraseña.join(""));
 alert("Su contraseña es:  " + contraseña.join(""));
 
 }
-
-
-
-}
+*/
